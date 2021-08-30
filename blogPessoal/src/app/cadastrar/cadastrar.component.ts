86d1a863bfcd5741e1262770721ce97b5324cdff
Jefferson-devs <jeffersonsoares.s@yahoo.com>
2021-08-30 15:03:46 -0300

    Consumo de API - Login e Cadastro

--- blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
+  confirmarSenha: string
+  tipoUsuario: string
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
   }
 
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar() {
+
+    if(this.user.nome.length<5){
+      alert("Preencha o nome com pelo menos 5 caracteres")
+    }
+
+    if(this.user.usuario.indexOf("@") == -1 || this.user.usuario.indexOf(".") == -1){
+      alert("Preencha o campo usuario com pelo menos um @ e um .")
+    }
+
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha.length<8){
+      alert("Preencha o campo senha com pelo menos 8 caracteres")
+    }
+
+    if (this.user.senha != this.confirmarSenha) {
+      alert('A senhas estão incorretas.')
+    } else {
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/entrar'])
+        alert('Usuário cadastrado com sucesso!')
+      })
+    }
+  }
 }
--- blogPessoal/src/app/model/User.ts
+++ blogPessoal/src/app/model/User.ts
@@ -2,7 +2,6 @@ import { Postagem } from "./Postagem"
 
 export class User{
     public id: number
-
     public nome: string
     public usuario: string
     public senha: string
