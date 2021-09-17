5fa4393b5f873847e3feb9ebc321409efde82504
Jefferson-devs <jeffersonsoares.s@yahoo.com>
2021-09-17 11:07:06 -0300

    Alertas reutilizaveis implementados

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component'
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
 import { UserEditComponent } from './edit/user-edit/user-edit.component';
+import { AlertaComponent } from './alerta/alerta.component';
 
 
 @NgModule({
@@ -35,7 +36,8 @@ import { UserEditComponent } from './edit/user-edit/user-edit.component';
     TemaDeleteComponent,
     PostagemEditComponent,
     PostagemDeleteComponent,
-    UserEditComponent
+    UserEditComponent,
+    AlertaComponent
   ],
 
   imports: [
--- blogPessoal/src/app/cadastrar/cadastrar.component.ts
+++ blogPessoal/src/app/cadastrar/cadastrar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../model/User';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -16,7 +17,8 @@ export class CadastrarComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private alertas: AlertasService
   ) { }
 
   ngOnInit() {
@@ -34,26 +36,26 @@ export class CadastrarComponent implements OnInit {
   cadastrar() {
 
     if(this.user.nome.length<5){
-      alert("Preencha o nome com pelo menos 5 caracteres")
+      this.alertas.showAlertSuccess("Preencha o nome com pelo menos 5 caracteres")
     }
 
     if(this.user.usuario.indexOf("@") == -1 || this.user.usuario.indexOf(".") == -1){
-      alert("Preencha o campo usuario com pelo menos um @ e um .")
+      this.alertas.showAlertSuccess("Preencha o campo usuario com pelo menos um @ e um .")
     }
 
     this.user.tipo = this.tipoUsuario
 
     if(this.user.senha.length<8){
-      alert("Preencha o campo senha com pelo menos 8 caracteres")
+      this.alertas.showAlertSuccess("Preencha o campo senha com pelo menos 8 caracteres")
     }
 
     if (this.user.senha != this.confirmarSenha) {
-      alert('A senhas estão incorretas.')
+      this.alertas.showAlertSuccess('A senhas estão incorretas.')
     } else {
       this.authService.cadastrar(this.user).subscribe((resp: User) => {
         this.user = resp
         this.router.navigate(['/entrar'])
-        alert('Usuário cadastrado com sucesso!')
+        this.alertas.showAlertSuccess('Usuário cadastrado com sucesso!')
       })
     }
   }
