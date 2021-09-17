a79d3d82a905cf894ff09d042934ce1f445513b2
Jefferson-devs <jeffersonsoares.s@yahoo.com>
2021-09-17 16:47:08 -0300

    adm implementado

--- blogPessoal/src/app/tema/tema.component.ts
+++ blogPessoal/src/app/tema/tema.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { Tema } from '../model/Tema';
+import { AlertasService } from '../service/alertas.service';
 import { TemaService } from '../service/tema.service';
 
 @Component({
@@ -13,15 +14,23 @@ export class TemaComponent implements OnInit {
 
   tema: Tema = new Tema()
   listaTemas: Tema[]
+  
 
   constructor(
     private router: Router,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private alertas : AlertasService
   ) { }
 
   ngOnInit() {
     if (environment.token == ''){
       this.router.navigate(['/entrar'])
+    }
+
+    if (environment.tipo != 'adm'){
+      this.alertas.showAlertInfo('Você precisa ser adm para acessar essa rota')
+      this.router.navigate(['/inicio'])
+
     }
 
     this.findAllTemas()
@@ -36,7 +45,7 @@ export class TemaComponent implements OnInit {
   cadastrar(){
     this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
       this.tema = resp
-      alert('Tema cadastrado com sucesso!')
+      this.alertas.showAlertSuccess('Tema cadastrado com sucesso!')
       this.findAllTemas()
       this.tema = new Tema()
     })
